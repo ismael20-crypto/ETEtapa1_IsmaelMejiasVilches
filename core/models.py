302ae528220039e6be580400464443d5fb2682b6
Dani-2206 <dlandero.alexander@gmail.com>
2021-06-09 17:21:51 -0400

    se a arreglado errores de los modelos

--- core/models.py
+++ core/models.py
@@ -2,33 +2,33 @@ from django.db import models
 
 # Create your models here.
 
-class genero(models.model):
-    id_genero= model.IntergerField(primaryKey=True, verbose_name='id genero')
-    genero= model.CharField(max_lenght=10,verbose_name='genero')
+class genero(models.Model):
+    id_genero= models.IntegerField(primary_key=True, verbose_name='id genero')
+    genero= models.CharField(max_length=10,verbose_name='genero')
 
 
     def __str__ (self):
 
         return(self.genero)
 
-def creacion_u(models.model):
-    nombre_f= model.CharField(min_lenght=3 , verbose_name='Nombre')
+class creacion_u(models.Model):
+    nombre_f= models.CharField(max_length=20 , verbose_name='Nombre')
 
-    apellido_f= model.CharField(min_lenght=5,verbose_name='Apellido')
+    apellido_f= models.CharField(max_length=20,verbose_name='Apellido')
 
-    edad_f= model.IntergerField(verbose_name='Edad')
+    edad_f=  models.IntegerField(verbose_name='Edad')
 
-    numero_t_f= model.IntergerField(max_lenght=9,verbose_name='Numero de celular')
+    numero_t_f=  models.IntegerField(verbose_name='Numero de celular')
 
-    fecha_nacimiento_f= model.DateField(verbose_name='Fecha de nacimiento')
+    fecha_nacimiento_f= models.DateField(verbose_name='Fecha de nacimiento')
 
-    genero_f= models.ForeignKey( genero ,on_delete=models.CASCADE)
+    genero_f= models.ForeignKey(genero ,on_delete=models.CASCADE)
 
-    email_f= model.CharField(min_lenght=2, verbose_name='Email')
+    email_f= models.CharField(max_length=40, verbose_name='Email')
 
-    nombre_usuario_f= model.CharField(primary_key=True, verbose_name='Nombre usuario')
+    nombre_usuario_f= models.CharField(primary_key=True, verbose_name='Nombre usuario', max_length=40)
 
-    contrasena= model.CharField(min_lenght='3',verbose_name='contrae')
+    contrasena= models.CharField(max_length=40,verbose_name='contraseña')
 
-    def __str__ (self)
+    def __str__ (self):
         return(self.nombre_usuario_f)
