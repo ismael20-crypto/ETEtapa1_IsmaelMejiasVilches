9f1b59e6cb78ca955f2e886ae44682e9eab62dad
Dani-2206 <dlandero.alexander@gmail.com>
2021-06-10 01:24:25 -0400

    implementacion de CRUD

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
+from .models import creacion_u
+from .forms import formulario_c
 
 # Create your views here.
 
@@ -13,4 +15,26 @@ def galeria(request):
 def quienes_s(request):
 
     return render (request , 'quienes_somos.html')
+
+def form_creacion(request):
+
+    if request.method=="POST":
+        formulario_c=creacion_u(request.POST)
+        if formulario_c.is_valid():
+           formulario_c.save()
+           return redirect('pagina_principal')
+    else:
+        formulario_c=creacion_u(request.POST)
+    
+    return render(request, 'core(2)/creacion_de_usuario', {'formulario_c': formulario_c})
+
+
+
+
+
+
+
+
+
+
 
