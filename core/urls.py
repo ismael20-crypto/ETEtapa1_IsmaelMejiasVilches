2ea52b49119c2cd15293dfe2a5292207eae4ed70
ismael20-crypto <ismaelmejias2000@gmail.com>
2021-06-18 21:09:31 -0400

    se hicieron cambios seralizers

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from .views import pagina_p,galeria,quienes_s,form_creacion,mostrar_u,modificar,eliminar
+form_del_Vehiculo, lista Vehiculos
 
 urlpatterns = [
     path('',pagina_p, name="pagina_principal"),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('Mostrar_usuario',mostrar_u, name="mostrar_u"),
     path('modificar/<id>',modificar,name="modificar"),
     path('elimina/<id>',eliminar,name="eliminar")
+    path('lista_vehiculos', lista_vehiculos, name="lista_vehiculos")
 ]
